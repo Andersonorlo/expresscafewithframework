0240aa91edbb66a98724dba22270b25e7b07cf1c
Andersonorlo <anderson_ordonezl@soy.sena.edu.co>
2025-09-09 18:31:37 -0500

    se sube archivo producto configurado con sus funciones

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+
+    public function categoria(){
+        return $this->beLongsTo(Categoria::class);
+    }
+    public function unidad (){
+        return $this->beLongsTo(Unidad::class);
+    }
+
+    public function usuario(){
+        return $this->beLongsTo(Usuario::class, 'user_id');
+    }
 }
